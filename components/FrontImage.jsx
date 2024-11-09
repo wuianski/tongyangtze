0b03ff37a1fabdb55a9541daed17a7b7ba1e6591
wuianski <wuiyeh@gmail.com>
2024-11-09 16:59:19 +0800

    install sharp for production

--- components/FrontImage.jsx
+++ components/FrontImage.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React, { useState } from "react";
 /* Next */
 import Image from "next/image";
 /* MUI */
@@ -8,47 +9,48 @@ import { Box } from "@mui/material";
 import { motion } from "framer-motion";
 
 export default function FrontImage({ image }) {
+  const [loading, setLoading] = useState(true);
+
   const variants = {
     hidden: {
       opacity: 0,
     },
-    visible: (custom) => ({
+    visible: {
       opacity: 1,
-      // y: 0,
-      transition: { delay: custom * 0.3, duration: 1.3 },
-    }),
+      // transition: { delay: custom * 0.3, duration: 0.6 },
+      transition: {
+        duration: 1.2,
+        ease: "easeIn",
+        ease: [0.17, 0.67, 0.83, 0.67],
+      },
+    },
   };
 
   return (
     <>
-      <motion.div
-        custom={1}
-        variants={variants}
-        initial="hidden"
-        animate="visible"
-      >
-        <Box>
-          <Box
-            sx={{
-              position: "fixed",
-              top: 0,
-              left: 0,
-              zIndex: -1,
-              width: "100vw",
-              height: "100vh",
-            }}
-          >
-            <Image
-              src={image}
-              alt="front page image"
-              fill={true}
-              quality={100}
-              style={{ objectFit: "cover", objectPosition: "center" }}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw"
-              priority={true}
-              placeholder="blur"
-            />
-          </Box>
+      <motion.div variants={variants} initial="hidden" animate="visible">
+        <Box
+          sx={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            zIndex: -1,
+            width: "100vw",
+            height: "100vh",
+          }}
+        >
+          <Image
+            src={image}
+            alt="front page image"
+            fill={true}
+            quality={100}
+            style={{ objectFit: "cover", objectPosition: "center" }}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw"
+            // priority={true}
+            placeholder="blur"
+            loading="lazy"
+            onLoad={() => setLoading(false)}
+          />
         </Box>
       </motion.div>
     </>
--- next.config.js
+++ next.config.js
@@ -2,16 +2,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: [
+      'dev.tongyangtze.com',
+    ],
     minimumCacheTTL: 60,
   },
   reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
+  // webpack(config) {
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     use: ['@svgr/webpack'],
+  //   });
+  //   return config;
+  // },
 }
 
 module.exports = (nextConfig);
